c01a27f11bdcab8751d8da373dcbf21aaa738c78
yavuzerbas <78691661+yavuzerbas@users.noreply.github.com>
2022-02-14 20:33:01 +0300

    flutter2

---
+++ departmentcourses/android/app/src/main/kotlin/com/example/departmentcourses/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.departmentcourses
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ departmentcourses/lib/Course.dart
@@ -0,0 +1,11 @@
+class Course {
+  String _courseName = "";
+
+  String getCourseName() {
+    return this._courseName;
+  }
+
+  void setCourseName(String courseName) {
+    this._courseName = courseName;
+  }
+}
